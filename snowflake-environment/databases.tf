88c8fc0a717b53d2a71501786687fe55cce08e25
high5technologies <high5technologies@gmail.com>
2022-11-07 09:00:57 -0600

    user password to env var

--- snowflake-environment/databases.tf
+++ snowflake-environment/databases.tf
@@ -1,6 +1,6 @@
 resource "snowflake_database" "raw" {
   provider                    = snowflake.terraform_sysadmin
   name                        = "${var.env}_RAW"
-  comment                     = "RAW Database "
+  comment                     = "RAW Database comment"
   data_retention_time_in_days = 3
 }
