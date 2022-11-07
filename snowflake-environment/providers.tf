6c704df72dcef94415e0de76e3a4f18a2b8a0f30
high5technologies <high5technologies@gmail.com>
2022-11-06 22:25:46 -0600

    snowflake connection as env vars

--- snowflake-environment/providers.tf
+++ snowflake-environment/providers.tf
@@ -13,17 +13,17 @@ terraform {
 provider "snowflake" {
     alias       = "terraform_sysadmin"
     role        = "TERRAFORM_SYSADMIN"
-    account     = var.snowflake_account
-    region      = var.snowflake_region
-    username    = var.snowflake_username
-    private_key = var.snowflake_terraform_private_key
+    #account     = var.snowflake_account
+    #region      = var.snowflake_region
+    #username    = var.snowflake_username
+    #private_key = var.snowflake_terraform_private_key
 }
 
 provider "snowflake" {
     alias       = "terraform_security"
     role        = "TERRAFORM_SECURITY"
-    account     = var.snowflake_account
-    region      = var.snowflake_region
-    username    = var.snowflake_username
-    private_key = var.snowflake_terraform_private_key
+    #account     = var.snowflake_account
+    #region      = var.snowflake_region
+    #username    = var.snowflake_username
+    #private_key = var.snowflake_terraform_private_key
 }
